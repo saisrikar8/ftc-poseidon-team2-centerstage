f71a547a01d2012308adbcb0dd472a148139def0
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2023-10-23 19:33:32 -0700

    Update RedTopAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedTopAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedTopAutonomous.java
@@ -7,16 +7,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.openftc.apriltag.AprilTagDetection;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 
-import java.util.ArrayList;
+
 
 @Autonomous(name = "RedTopAutonomous")
 public class RedTopAutonomous extends LinearOpMode {
