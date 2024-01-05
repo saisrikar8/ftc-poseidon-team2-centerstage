12cb3fd7b04cccbeb6e6747533a208ab93676022
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2024-01-04 22:03:19 -0800

    removing unneeded packages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueTopAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueTopAutonomous.java
@@ -7,8 +7,6 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
